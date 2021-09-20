3727ce63086b4cdd38339a958750eb665676875e
vignesh679 <39797027+vignesh679@users.noreply.github.com>
2021-09-20 23:10:13 +0530

    Add files via upload

---
+++ canvas/canvas.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const c = canvas.getContext("2d");
+let boxAreas = []
+function drawBoard(){
+	let maxBoxSide = canvas.height/3
+	let xOffset = canvas.width/2 - maxBoxSide * 1.5
+	let yOffset = canvas.height/2 - maxBoxSide * 1.5
+	for(let i=0; i<4; i++) {
+		c.moveTo(xOffset, yOffset + maxBoxSide * i);
+		c.lineTo(xOffset + maxBoxSide * 3, yOffset + maxBoxSide * i);
+		c.moveTo(xOffset + maxBoxSide * i, yOffset);
+		c.lineTo(xOffset + maxBoxSide * i, yOffset + maxBoxSide * 3);
+		boxAreas.push([
+			xOffset + maxBoxSide * i,
+			yOffset + maxBoxSide * i,
+			xOffset + maxBoxSide * (i + 1),
+			yOffset + maxBoxSide * (i + 1),
+		])
+	}
+	c.strokeStyle = "black";
+	c.stroke();
+}
+
+// canvas.addEventListener('mousemove',function(moved){
+// 		console.log(moved);
+// })
+drawBoard();    
